127db9501a8d0379b417d9079914bd3b55a4acf1
Dante <dante.uchofen@usil.pe>
2023-10-18 15:47:21 -0500

    dante2

--- banco/Main.java
+++ banco/Main.java
@@ -13,9 +13,9 @@ public class Main {
 		cuentaSueldo.setSaldoDisponible(1500);
 		cuentaFree.setSaldoDisponible(20000);
 		
-		CuentaBancaria[] cuentasBancarias= {cuentaFree, cuentaSueldo, cuentaCorriente1, cuentaCorriente2};
-		
+		CuentaBancaria[] cuentasBancarias= {cuentaFree, cuentaSueldo, cuentaCorriente1, cuentaCorriente2, cuentaCTS};		
 		cliente.setCuentas(cuentasBancarias);
+		
 		System.out.println(cliente.toString());
 	}
 }
