5e91649773e5d4b87fea6a19143b93c4ddb745e7
valentinamj <142445272+valentinamj@users.noreply.github.com>
2023-10-20 01:40:27 -0500

    Add files via upload
    Valentina Julca

--- banco/Main.java
+++ banco/Main.java
@@ -1,5 +1,7 @@
 package banco;
 
+import java.util.Date;
+
 public class Main {
 	
 	public static void main(String[] args) {
@@ -7,9 +9,11 @@ public class Main {
 			
 		String[] beneficiosFree = {"Acceso a teatro", "Acceso a restaurantes", "Acceso a cursos"};
 		
+		String[] beneficiosSueldo = {"tiene un 15% en ropa y calzado en Ripley" , "Obtiene una promocion de 1/4 de pollo en pardos a 15.90"," tiene un 25% mensual en SmartFit", "Obtiene un 10% en concierto y eventos en Teletickect, tiene un 30% en Booking en reserva de hoteles"};
+		
 		CuentaBancaria cuentaFree=new CuentaFree("385728935892", "Cuenta free", "9582893293", beneficiosFree, false);
 
-		CuentaBancaria cuentaSueldo=new CuentaSueldo("53252624664", "Cuenta sueldo", "75474434", null, "Inkafarma");
+		CuentaBancaria cuentaSueldo=new CuentaSueldo("53252624664", "Cuenta sueldo", "75474434", beneficiosSueldo, "Inkafarma");
 		
 		CuentaBancaria cuentaCorriente1=new CuentaCorriente("5745754754", "Cuenta corriente 1", "34283473284", 5);
 		
@@ -17,6 +21,18 @@ public class Main {
 		
 		CuentaBancaria cuentaCTS=new CuentaCTS("6436344", "Cuenta corriente 2", "531511447", 50000 );
 		
+		EventoBancario eventobancario1 =new EventoBancario(new Date(),"La gran revelacion","Lanzamientode un nuevo producto");
+		
+		EventoBancario eventobancario2 =new EventoBancario(new Date(),"Revolucion de Tasas","Cambios en las tasas de interes");
+		
+		EventoBancario eventobancario3 =new EventoBancario(new Date(),"Banco digital 2.0","Implementacion de tecnologia financiera");
+		
+		EventoBancario eventobancario4 =new EventoBancario(new Date(),"Nueva era, nuevas lideranzas","Cambios en la Alta dirrecion");
+		
+		EventoBancario eventobancario5=new EventoBancario(new Date(),"Innovando con responsabilidad","Cambios en las Politicas y regulaciones");
+	
+		EventoBancario[] eventosbancarios= {eventobancario1, eventobancario2,eventobancario3,eventobancario4,eventobancario5};
+		cliente.setEventoBancarios(eventosbancarios);
 		
         cuentaSueldo.setSaldoDisponible(1500);
 		cuentaFree.setSaldoDisponible(20000);
